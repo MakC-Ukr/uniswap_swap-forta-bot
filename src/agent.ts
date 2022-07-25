7dd74d734e26dac3e12c99228ee1f8e11b309a03
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-07-25 23:18:18 +0530

    feat: can verify if the contract is Uniswap v3 contract or not

--- src/agent.ts
+++ src/agent.ts
@@ -9,15 +9,13 @@ import {
   getJsonRpcUrl
 } from "forta-agent";
 var ethers = require('ethers');
+import dataJson from './data.json'; 
 
-const SWAP_EVENT = "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)";
 
-const UniswapV3FactoryContractAddress: string = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
-const FactoryABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint24","name":"fee","type":"uint24"},{"indexed":true,"internalType":"int24","name":"tickSpacing","type":"int24"}],"name":"FeeAmountEnabled","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"oldOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnerChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":true,"internalType":"uint24","name":"fee","type":"uint24"},{"indexed":false,"internalType":"int24","name":"tickSpacing","type":"int24"},{"indexed":false,"internalType":"address","name":"pool","type":"address"}],"name":"PoolCreated","type":"event"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint24","name":"fee","type":"uint24"}],"name":"createPool","outputs":[{"internalType":"address","name":"pool","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint24","name":"fee","type":"uint24"},{"internalType":"int24","name":"tickSpacing","type":"int24"}],"name":"enableFeeAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint24","name":"","type":"uint24"}],"name":"feeAmountTickSpacing","outputs":[{"internalType":"int24","name":"","type":"int24"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"},{"internalType":"uint24","name":"","type":"uint24"}],"name":"getPool","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"parameters","outputs":[{"internalType":"address","name":"factory","type":"address"},{"internalType":"address","name":"token0","type":"address"},{"internalType":"address","name":"token1","type":"address"},{"internalType":"uint24","name":"fee","type":"uint24"},{"internalType":"int24","name":"tickSpacing","type":"int24"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"setOwner","outputs":[],"stateMutability":"nonpayable","type":"function"}]; 
-const PoolABI = [{"inputs":[],"name":"fee","outputs":[{"internalType":"uint24","name":"","type":"uint24"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}];
+
 let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
 
-let v3Factory = new ethers.Contract(UniswapV3FactoryContractAddress, FactoryABI, provider);
+let v3Factory = new ethers.Contract(dataJson.UniswapV3FactoryContractAddress, dataJson.FactoryABI, provider);
 
 
 const handleTransaction: HandleTransaction = async (
@@ -25,17 +23,15 @@ const handleTransaction: HandleTransaction = async (
 ) => {
   const findings: Finding[] = [];
   const swapEvents = txEvent.filterLog(
-    SWAP_EVENT
+    dataJson.SWAP_EVENT
   );  
-
-  console.log(swapEvents.length);
 
   for (const swapEvent of swapEvents) {
     const poolAddress : string = swapEvent['address'];
-    let poolContract = new ethers.Contract(poolAddress, PoolABI, provider);
-    const token0 = await poolContract.token0();
-    const token1 = await poolContract.token1();
-    const fee = await poolContract.fee();
+    let poolContract = new ethers.Contract(poolAddress, dataJson.PoolABI, provider);
+    let token0 = await poolContract.token0();
+    let token1 = await poolContract.token1();
+    let fee = await poolContract.fee();
     let temp: string =  await v3Factory.getPool(token0, token1, fee);
 
     if(temp.toLowerCase() == poolAddress.toLowerCase()){
