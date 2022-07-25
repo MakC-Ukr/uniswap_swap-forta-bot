bff99912512fa87c19fbf8dbcdb84ee3d88218d6
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-07-25 21:02:09 +0530

    feat: added event logging of a V3 Swap

--- src/agent.ts
+++ src/agent.ts
@@ -8,61 +8,38 @@ import {
   FindingType,
 } from "forta-agent";
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
-let findingsCount = 0;
+const SWAP_EVENT = "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)";
+const UniswapV3FactoryContractAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   const findings: Finding[] = [];
+  const swapEvents = txEvent.filterLog(
+    SWAP_EVENT
+  );  
+  swapEvents.forEach((swapEvent) => {
+    const poolAddress = swapEvent['address'];
+    const { sender, recipient, amount0, amount1, sqrtPriceX96, liquidity, tick } = swapEvent.args;
+    // console.log({sender, recipient, amount0, amount1, sqrtPriceX96, liquidity, tick});
+    // console.log({sender, recipient});
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
-
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
-  );
-
-  tetherTransferEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
-
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
-      findings.push(
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to,
-            from,
-          },
-        })
-      );
-      findingsCount++;
-    }
+    findings.push(
+      Finding.fromObject({
+        name: "Swap on Uniswap V3",
+        description: `A Uniswap V3 swapjust took place.`,
+        alertId: "UNIS-1",
+        severity: FindingSeverity.Low,
+        type: FindingType.Info,
+        metadata: {},
+      })
+    );
   });
 
   return findings;
 };
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
 
 export default {
   handleTransaction,
-  // handleBlock
 };
