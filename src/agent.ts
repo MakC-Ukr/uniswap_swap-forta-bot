c9e3d647b61c80049e5781bc29f27bd4cba7cc27
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-07-26 10:45:14 +0530

    feat: added caching and simultaneous async calls to get data

--- src/agent.ts
+++ src/agent.ts
@@ -10,13 +10,14 @@ import {
 } from "forta-agent";
 var ethers = require('ethers');
 import dataJson from './data.json'; 
+import LRU from "lru-cache";
+
+const cache: LRU<string, string[]> = new LRU<string, string[]>({ max: 10000 });
 
 
 
 let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
-
 let v3Factory = new ethers.Contract(dataJson.UniswapV3FactoryContractAddress, dataJson.FactoryABI, provider);
-
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
@@ -28,17 +29,28 @@ const handleTransaction: HandleTransaction = async (
 
   for (const swapEvent of swapEvents) {
     const poolAddress : string = swapEvent['address'];
-    let poolContract = new ethers.Contract(poolAddress, dataJson.PoolABI, provider);
-    let token0 = await poolContract.token0();
-    let token1 = await poolContract.token1();
-    let fee = await poolContract.fee();
-    let temp: string =  await v3Factory.getPool(token0, token1, fee);
+    let [token0, token1, fee] : string[] = [];
+    
+    if(cache.has(poolAddress)){
+      let poolData : string[] = cache.get(poolAddress)!;
+      console.log(poolData);
+      token0 = poolData[0];
+      token1 = poolData[1];
+      fee = poolData[2];  
+    }
+    else{
+      let poolContract = new ethers.Contract(poolAddress, dataJson.PoolABI, provider);
+      [token0, token1, fee] = await Promise.all([poolContract.token0(), poolContract.token1(), poolContract.fee()]);
+      let addToCache: string[] = [token0, token1, fee];
+      cache.set(poolAddress, addToCache);
+    }  
 
+    let temp: string =  await v3Factory.getPool(token0, token1, fee);
     if(temp.toLowerCase() == poolAddress.toLowerCase()){
       findings.push(
         Finding.fromObject({
           name: "Swap on Uniswap V3",
-          description: `A Uniswap V3 swapjust took place.`,
+          description: `A Uniswap V3 swap just took place.`,
           alertId: "UNIS-1",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
