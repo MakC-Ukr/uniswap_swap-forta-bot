693bad7625aefac4eb417277e1498156266bf7c8
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-07-26 18:15:18 +0530

    fix: post-review edits

--- README.md
+++ README.md
@@ -4,9 +4,6 @@
 
 This agent detects any swaps made with Uniswap V3. It only detects swaps done by Pools deployed by Uniswap v3 Factory Contract (0x1F98431c8aD98523631AE4a59f267346ea31F984).
 
-## Deploying on other chains
-In order to deploy the agent on other chains, the RPC URL must be provided as `jsonRpcUrl` to `./forta.config.json` and the value of `chainId` must be corrected in `./package.json`. Along with that UniswapV3's factory contract address must be changed in data.json. 
-
 ## Supported Chains
 
 - Ethereum Mainnet (chainId: 1)
@@ -27,5 +24,3 @@ The agent behaviour can be verified with the following transactions:
 - [0xf46f0eb9502b9f361c6b24c4f56f7ecb93fcc90c9805ccb427942cea47d8cf18](https://etherscan.io/tx/0xf46f0eb9502b9f361c6b24c4f56f7ecb93fcc90c9805ccb427942cea47d8cf18) (Swap 127,978 USDC For 84.46 Ether On Uniswap **V3**) - should return `1 finding`
 - [0x020f12508ee869fc5eb7306eea45b7865ec2ea66bd9e0f9176ee0e305ac3bc43](https://etherscan.io/tx/0x020f12508ee869fc5eb7306eea45b7865ec2ea66bd9e0f9176ee0e305ac3bc43) (Swap 0.24 Ether For 1,025 LOOKS On Uniswap **V3**) - should return `1 finding`
 - [0x03f883f63841b3498fe83ac6f47b0e1ef5b4608fecae0ab4f9471da5e0d0b033](https://etherscan.io/tx/0x03f883f63841b3498fe83ac6f47b0e1ef5b4608fecae0ab4f9471da5e0d0b033) (Swap 5,736 USDC For 176,197 TSUKA On Uniswap **V2**) - should return `0 findings`
-
-To check a certain transaction, run `npm run tx TRANSACTION_HASH`
--- src/agent.ts
+++ src/agent.ts
@@ -1,7 +1,5 @@
 import {
-  BlockEvent,
   Finding,
-  HandleBlock,
   HandleTransaction,
   TransactionEvent,
   FindingSeverity,
@@ -12,50 +10,64 @@ var ethers = require("ethers");
 import dataJson from "./data.json";
 import LRU from "lru-cache";
 
-const cache: LRU<string, string[]> = new LRU<string, string[]>({ max: 10000 });
+export function provideHandleTransaction(
+  SWAP_EVENT: string,
+  PoolABI: any[],
+  UniswapV3FactoryContractAddress: string,
+  FactoryABI: any[]
+): HandleTransaction {
+  const cache: LRU<string, string[]> = new LRU<string, string[]>({ max: 10000 });
 
-let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
-let v3Factory = new ethers.Contract(dataJson.UniswapV3FactoryContractAddress, dataJson.FactoryABI, provider);
+  let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
+  let v3Factory = new ethers.Contract(UniswapV3FactoryContractAddress, FactoryABI, provider);
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = [];
-  const swapEvents = txEvent.filterLog(dataJson.SWAP_EVENT);
+  return async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+    const swapEvents = txEvent.filterLog(SWAP_EVENT);
 
-  for (const swapEvent of swapEvents) {
-    const poolAddress: string = swapEvent["address"];
-    let [token0, token1, fee]: string[] = [];
+    for (const swapEvent of swapEvents) {
+      const poolAddress: string = swapEvent["address"];
+      let [token0, token1, fee]: string[] = [];
 
-    if (cache.has(poolAddress)) {
-      let poolData: string[] = cache.get(poolAddress)!;
-      console.log(poolData);
-      token0 = poolData[0];
-      token1 = poolData[1];
-      fee = poolData[2];
-    } else {
-      let poolContract = new ethers.Contract(poolAddress, dataJson.PoolABI, provider);
-      [token0, token1, fee] = await Promise.all([poolContract.token0(), poolContract.token1(), poolContract.fee()]);
-      let addToCache: string[] = [token0, token1, fee];
-      cache.set(poolAddress, addToCache);
+      if (cache.has(poolAddress)) {
+        let poolData: string[] = cache.get(poolAddress)!;
+        token0 = poolData[0];
+        token1 = poolData[1];
+        fee = poolData[2];
+      } else {
+        let poolContract = new ethers.Contract(poolAddress, PoolABI, provider);
+        [token0, token1, fee] = await Promise.all([poolContract.token0(), poolContract.token1(), poolContract.fee()]);
+        let addToCache: string[] = [token0, token1, fee];
+        cache.set(poolAddress, addToCache);
+      }
+
+      let temp: string = await v3Factory.getPool(token0, token1, fee);
+      if (temp.toLowerCase() == poolAddress.toLowerCase()) {
+        findings.push(
+          Finding.fromObject({
+            name: "Swap on Uniswap V3",
+            description: `A Uniswap V3 swap just took place.`,
+            alertId: "UNIS-1",
+            protocol:"Uniswap",
+            severity: FindingSeverity.Low,
+            type: FindingType.Info,
+            metadata: {
+              "network":txEvent.network.toString()
+            },
+          })
+        );
+      }
     }
 
-    let temp: string = await v3Factory.getPool(token0, token1, fee);
-    if (temp.toLowerCase() == poolAddress.toLowerCase()) {
-      findings.push(
-        Finding.fromObject({
-          name: "Swap on Uniswap V3",
-          description: `A Uniswap V3 swap just took place.`,
-          alertId: "UNIS-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {},
-        })
-      );
-    }
-  }
-
-  return findings;
-};
+    return findings;
+  };
+}
 
 export default {
-  handleTransaction,
+  handleTransaction: provideHandleTransaction(
+    dataJson.SWAP_EVENT,
+    dataJson.PoolABI,
+    dataJson.UniswapV3FactoryContractAddress,
+    dataJson.FactoryABI
+  ),
 };
