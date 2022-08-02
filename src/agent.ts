3487b93efda4f8b89a5f3bbd9f4039e869216fe8
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-02 18:18:50 +0530

    fix: using ethers create2address

--- src/agent.ts
+++ src/agent.ts
@@ -7,18 +7,13 @@ import {
   ethers,
   getJsonRpcUrl,
 } from "forta-agent";
-import {SWAP_EVENT, UNISWAP_V3_FACTORY_CONTRACT_ADDRESS, UNISWAP_POOL_CONTRACT_CREAT_CODE, FACTORY_ABI, POOL_ABI} from "./constants"
+import {SWAP_EVENT, UNISWAP_V3_FACTORY_CONTRACT_ADDRESS, UNISWAP_POOL_CONTRACT_CREAT_CODE_HASH, POOL_ABI} from "./constants"
 import LRU from "lru-cache";
 
 function getPoolAddress(token0 : string, token1 : string, fee: string) {   
     let abiCoder = new ethers.utils.AbiCoder();
     let salt : string = ethers.utils.keccak256(abiCoder.encode(["address", "address", "uint24"], [token0, token1, fee]));
-    let hash : string = ethers.utils.keccak256(
-      ethers.utils.solidityPack(
-        ["bytes1","address","uint","bytes32"],
-        ["0xff", UNISWAP_V3_FACTORY_CONTRACT_ADDRESS, salt, ethers.utils.keccak256(UNISWAP_POOL_CONTRACT_CREAT_CODE)]
-      ));
-    hash = "0x"+hash.slice(26); // (64+2)-40
+    let hash : string = ethers.utils.getCreate2Address(UNISWAP_V3_FACTORY_CONTRACT_ADDRESS, salt, UNISWAP_POOL_CONTRACT_CREAT_CODE_HASH);  
     return hash;
 }
 
@@ -26,13 +21,9 @@ export function provideHandleTransaction(
   swapEventParam: string,
   poolAbi: any[],
   uniswapV3FactoryContractAddress: string,
-  factoryABI: any[]
 ): HandleTransaction {
   const cache: LRU<string, string[]> = new LRU<string, string[]>({ max: 10000 });
-
   let provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
-  let v3Factory = new ethers.Contract(uniswapV3FactoryContractAddress, factoryABI, provider);
-
   return async (txEvent: TransactionEvent) => {
     const findings: Finding[] = [];
     const swapEvents = txEvent.filterLog(swapEventParam);
@@ -94,6 +85,5 @@ export default {
   handleTransaction: provideHandleTransaction(
     SWAP_EVENT,
     POOL_ABI, 
-    UNISWAP_V3_FACTORY_CONTRACT_ADDRESS, 
-    FACTORY_ABI),
+    UNISWAP_V3_FACTORY_CONTRACT_ADDRESS),
 };
